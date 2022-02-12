ba46e7e760ba1cde0906f7e04ec6946d579440e0
Kaushal Sharma <kaushalsharma880@gmail.com>
2022-02-12 09:41:59 +0530

    s3 storage and test

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -4,15 +4,28 @@ import pytest
 from pgbackup import storage
 
 
-def test_storing_file_locally():
+@pytest.fixture
+def infile():
+    f = tempfile.TemporaryFile()
+    f.write(b'Testing')
+    f.seek(0)
+    return f
+
+
+def test_storing_file_locally(infile):
     """
     Writes content from one file-like to another.
     """
-    infile = tempfile.TemporaryFile('r+b')
-    infile.write(b'Testing')
-    infile.seek(0)
-
     outfile = tempfile.NamedTemporaryFile(delete=False)
     storage.local(infile, outfile)
     with open(outfile.name, 'rb') as f:
         assert f.read() == b'Testing'
+
+
+def test_storing_file_on_s3(mocker, infile):
+    """
+    Writes content from one file-like to S3
+    """
+    client = mocker.Mock()
+    storage.s3(client, infile, 'bucketname', 'file-name')
+    client.upload_fileobj.assert_called_with(infile, 'bucketname', 'file-name')
